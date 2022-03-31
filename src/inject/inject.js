0f38d29a397934ee4ae59903dd0bf67d69c6ec65
Colton Robbins <coltondr@gmail.com>
2022-03-31 00:53:23 -0500

    cleaned up code a bit

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,24 +1,15 @@
 chrome.extension.sendMessage({}, function () {
 
-
+    // all pet images at a working url
 	const imageArray = ["https://i.imgur.com/yJQySyu.jpg", "https://i.imgur.com/LkFrJeK.jpg", "https://i.imgur.com/KepRw7I.jpg", "https://i.imgur.com/IIEpZK3.jpg", "https://i.imgur.com/Yve9w1K.jpg", "https://i.imgur.com/N3aW4bL.jpg", "https://i.imgur.com/jI5DadO.jpg", "https://i.imgur.com/Qu5fmRh.jpg", "https://i.imgur.com/0YBfUwS.jpg", "https://i.imgur.com/I5q9mWp.jpg", "https://i.imgur.com/lz4WULX.jpg", "https://i.imgur.com/k9974BC.jpg", "https://i.imgur.com/mrmjWtk.jpg"];
 
-
-
+    // gets all img elements on the webpage
 	let images = document.getElementsByTagName('img');
 
+	// each pet image is reassigned to a random pet image url
 	for (let i = 0; i < images.length; i++) {
 		images[i].srcset = imageArray[Math.floor(Math.random() * (imageArray.length))];
 	};
 
-	console.log(images)
-	// ----------------------------------------------------------
-	// This part of the script triggers when page is done loading
-	console.log("Hello world!!!!!!!");
-	// ----------------------------------------------------------
-
 
 });
-// }, 10);
-// });
-
